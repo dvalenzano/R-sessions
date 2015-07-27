eab80bb078db5a46270329c3dfd586b3cf58be66
dario valenzano <drvalenzano@gmail.com>
2015-07-27 12:00:02 +0200

    Update control-survG.R

--- control-survG.R
+++ control-survG.R
@@ -83,3 +83,30 @@ fitf <- survfit(formula=Surv(time,status)~X40077, data=g2f)
 plot(fitf, xlim=c(0,500), ylab="Fraction survived", xlab="Time (days)", lwd=c(3,3,3), col=c(2:4), main="G-cross females marker 40077")
 legend(400,.9, legend=c("sl/sl","sl/ll", "ll/ll"), lwd=c(3,3,3), col=c(2:4))
 
+
+######### CODE TO GENERATE THE STATISTICS FOR FIGURE 6E #DONE ON 27-Jul-2015###########
+## To run the statistics between sl/sl and ll/ll I subset g2 for homozygous individuals only
+
+g3.1 <- subset(g2[,1:3])
+g3.1$X46347 <- g2$X46347
+g3.1$time <- g2$time
+g3.1$status <- g2$status
+g3.2 <- subset(g3.1,X46347!=1)
+
+fit46347_2 <- survfit(formula=Surv(time,status)~X46347, data=g3.2)
+plot(fit46347_2, xlim=c(0,500), ylab="Fraction survived", xlab="Time (days)", lwd=c(3,3), col=c(2,4), main="G-cross marker 46347")
+legend(400,.9, legend=c("sl/sl", "ll/ll"), lwd=c(2.5,2.5), col=c(2,4))
+
+# The following command returns the p-value:
+survdiff(formula=Surv(time,status)~X46347, data=g3.2)
+
+#Call:
+#  survdiff(formula = Surv(time, status) ~ X46347, data = g3.2)
+
+#N Observed Expected (O-E)^2/E (O-E)^2/V
+#X46347=0 30       30     18.7      6.87      9.92
+#X46347=2 44       44     55.3      2.32      9.92
+
+#Chisq= 9.9  on 1 degrees of freedom, p= 0.00164
+
+
